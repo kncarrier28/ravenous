e3e8b92115352e15e07f9b49375d7aa903f3202c
Kenton Carrier <kncarrier28@gmail.com>
2019-03-27 12:18:57 -0400

    Rendering Business

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,6 +4,15 @@ import Business from '../Business/Business.js';
 
 class BusinessList extends React.Component {
   render() {
-
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
   }
 }
