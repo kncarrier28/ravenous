1a53e5d39f301cdf512f274171cf2c459760d671
Kenton Carrier <kncarrier28@gmail.com>
2019-03-28 16:22:23 -0400

    Create businesses array

--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
 class App extends Component {
   render() {
     return (
       <div class="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList />
       </div>
     );
   }
