90efca314fb2721ffda49e168019e7753c448a34
Kenton Carrier <kncarrier28@gmail.com>
2019-03-28 11:47:02 -0400

    Render ravenous

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import SearchBar from './components/SearchBar/SearchBar.js';
 class App extends Component {
   render() {
     return (
-      
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>
     );
   }
 }
