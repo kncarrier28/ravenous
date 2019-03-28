ec89d47d3ced3fce73b70e185639db199361c860
Kenton Carrier <kncarrier28@gmail.com>
2019-03-28 16:27:16 -0400

    Modify Business.js using props

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,19 +6,19 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt={business.name}/>
+          <img src={this.props.business.imageSrc} alt={this.props.business.name}/>
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state} {business.zipCode}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipCode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
