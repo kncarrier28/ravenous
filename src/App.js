b8f6e87d2c30d5d74b5a632c16c91eb18bfa1e26
Kenton Carrier <kncarrier28@gmail.com>
2019-03-28 11:42:58 -0400

    Import BusinessList and SearchBar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+
 class App extends Component {
   render() {
     return (
